60d86ef6e418b9efda6bc6116531445a6c2e344a
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-09-14 17:22:19 +0300

    rewrite on hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,73 +1,78 @@
 // import Counter from 'components/Counter/Counter';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from 'components/Counter/FeedbackOptions';
 import Statistics from 'components/Counter/Statistics';
 import Section from 'components/Counter/Section';
 import Notification from 'components/Counter/Notification';
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
 
-  onLeaveFeedback = e => {
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function onLeaveFeedback(e) {
     const key = e.target.attributes.option.value;
-    this.setState(prevState => ({
-      [key]: prevState[key] + 1,
-    }));
-  };
+    switch (key) {
+      case 'good':
+        setGood(pS => pS + 1);
+        break;
+      case 'neutral':
+        setNeutral(pS => pS + 1);
+        break;
+      case 'bad':
+        setBad(pS => pS + 1);
+        break;
+      default:
+        break;
+    }
+  }
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  function countTotalFeedback() {
     return good + neutral + bad;
-  };
+  }
 
-  countPositiveFeedbackPercentage = () => {
-    if (!this.countTotalFeedback()) {
+  function countPositiveFeedbackPercentage() {
+    if (countTotalFeedback()) {
       return 0;
     }
     const posFeedback = Number(
-      (100 * (this.state.good / this.countTotalFeedback())).toFixed(2)
+      (100 * (good / countTotalFeedback())).toFixed(2)
     );
     return posFeedback;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div
-        style={{
-          width: '450px',
-          height: 'auto',
-          // minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}
-      >
-        <h1>React homework</h1>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
   }
+
+  return (
+    <div
+      style={{
+        width: '450px',
+        height: 'auto',
+        // minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <h1>React homework</h1>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </div>
+  );
 }
